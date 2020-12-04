d59f0d4dc1f6d10011419301430371e344c0df37
Odei Bretón <odeibreton@hotmail.com>
2020-12-04 22:25:16 +0000

    Repository functionality extracted

--- Checkout.PaymentGateway.Infrastructure/PaymentRepository.cs
+++ Checkout.PaymentGateway.Infrastructure/PaymentRepository.cs
@@ -8,70 +8,25 @@ using Microsoft.Extensions.Logging;
 
 namespace Checkout.PaymentGateway.Infrastructure
 {
-    public class PaymentRepository : IPaymentRepository
+    public class PaymentRepository : Repository, IPaymentRepository
     {
-        private readonly PaymentGatewayDbContext _dbContext;
         private readonly ILogger<PaymentRepository> _logger;
-        private IDbContextTransaction _transaction;
-
-        private IDbContextTransaction Transaction
-        {
-            get => _transaction;
-            set
-            {
-                if (_transaction != null)
-                {
-                    throw new InvalidOperationException("Cannot create a new transaction while another one is in progress.");
-                }
-
-                _transaction = value;
-            }
-        }
 
         public PaymentRepository(PaymentGatewayDbContext dbContext, ILogger<PaymentRepository> logger)
+            : base(dbContext, logger)
         {
-            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public Task<Payment> GetByIdAsync(PaymentId id)
         {
-            return _dbContext.Payments.FindAsync(id).AsTask();
+            return DbContext.Payments.FindAsync(id).AsTask();
         }
 
         public async Task CreateAsync(Payment payment)
         {
             await BeginTransactionAsync();
-            await _dbContext.Payments.AddAsync(payment);
-        }
-
-        public async Task SaveAsync()
-        {
-            if (Transaction is null)
-            {
-                return;
-            }
-
-            try
-            {
-                await Transaction.CommitAsync();
-            }
-            catch (Exception e)
-            {
-                await Transaction.RollbackAsync();
-                _logger.LogError(e, "Database transaction commit failed.");
-                throw;
-            }
-            finally
-            {
-                await Transaction.DisposeAsync();
-                Transaction = null;
-            }
-        }
-
-        private async Task BeginTransactionAsync()
-        {
-            Transaction = await _dbContext.Database.BeginTransactionAsync();
+            await DbContext.Payments.AddAsync(payment);
         }
     }
 }
