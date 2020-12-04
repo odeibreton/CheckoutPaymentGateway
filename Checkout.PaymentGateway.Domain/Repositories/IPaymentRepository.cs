f8d5a973b48edab84e7959a1dc328acfa36de09e
Odei Bretón <odeibreton@hotmail.com>
2020-12-04 22:14:55 +0000

    PaymentGatewayDbContext added and configured

---
+++ Checkout.PaymentGateway.Domain/Repositories/IPaymentRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkout.PaymentGateway.Domain.Repositories
+{
+    public interface IPaymentRepository
+    {
+        Task<Payment> GetByIdAsync(PaymentId id);
+        Task CreateAsync(Payment payment);
+        Task SaveAsync();
+    }
+}
---
+++ Checkout.PaymentGateway.Infrastructure/DbContext/PaymentGatewayDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Checkout.PaymentGateway.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Checkout.PaymentGateway.Infrastructure.DbContext
+{
+    public class PaymentGatewayDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public PaymentGatewayDbContext()
+            : base()
+        {
+        }
+
+        public PaymentGatewayDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Payment> Payments { get; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+    }
+}
