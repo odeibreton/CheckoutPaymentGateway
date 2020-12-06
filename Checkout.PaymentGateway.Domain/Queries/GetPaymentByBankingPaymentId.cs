68b919d1008927649c7f9b91bd80d5d4de40a742
Odei Bretón <odeibreton@hotmail.com>
2020-12-06 22:25:47 +0000

    GetPaymentById renamed to GetPaymentByBankingPaymentId

--- Checkout.PaymentGateway.Domain/Queries/GetPaymentByBankingPaymentId.cs
+++ Checkout.PaymentGateway.Domain/Queries/GetPaymentByBankingPaymentId.cs
@@ -5,7 +5,7 @@ using Checkout.PaymentGateway.Domain.Framework;
 
 namespace Checkout.PaymentGateway.Domain.Queries
 {
-    public class GetPaymentById : IQuery
+    public class GetPaymentByBankingPaymentId : IQuery
     {
         public string BankingPaymentId { get; set; }
     }
