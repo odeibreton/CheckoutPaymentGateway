fc146494b95716c4befb006be2d4d445e29d2d1b
Odei Bretón <odeibreton@hotmail.com>
2020-12-06 22:05:08 +0000

    BankingService and payment creating handling implemented and tested

---
+++ Checkout.PaymentGateway.Application/DTO/PaymentInformation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Checkout.PaymentGateway.Application.DTO
+{
+    public sealed class PaymentInformation
+    {
+        public string CardNumber { get; set; }
+        public int ExpityMonth { get; set; }
+        public int ExpiryYear { get; set; }
+        public string CVV { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+    }
+}
---
+++ Checkout.PaymentGateway.Application/Handlers/CreatePaymentHandler.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Checkout.PaymentGateway.Application.DTO;
+using Checkout.PaymentGateway.Application.Services;
+using Checkout.PaymentGateway.Domain;
+using Checkout.PaymentGateway.Domain.Commands;
+using Checkout.PaymentGateway.Domain.Framework;
+using Checkout.PaymentGateway.Domain.Repositories;
+using Microsoft.Extensions.Logging;
+
+namespace Checkout.PaymentGateway.Application.Handlers
+{
+    public sealed class CreatePaymentHandler : ICommandHandler<CreatePayment>
+    {
+        private readonly IPaymentRepository _paymentRepository;
+        private readonly IBankingService _bankingService;
+        private readonly ILogger<CreatePaymentHandler> _logger;
+
+        public CreatePaymentHandler(IPaymentRepository paymentRepository, IBankingService bankingService, ILogger<CreatePaymentHandler> logger)
+        {
+            _paymentRepository = paymentRepository ?? throw new ArgumentNullException(nameof(paymentRepository));
+            _bankingService = bankingService ?? throw new ArgumentNullException(nameof(bankingService));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task HandleAsync(CreatePayment command)
+        {
+            _ = command ?? throw new ArgumentNullException(nameof(command));
+
+            var paymentInformation = new PaymentInformation()
+            {
+                CardNumber = command.CardNumber,
+                ExpityMonth = command.ExpityMonth,
+                ExpiryYear = command.ExpiryYear,
+                CVV = command.CVV,
+                Amount = command.Amount,
+                Currency = command.Currency
+            };
+
+            var bankingPaymentId = await SubmitPaymentAsync(paymentInformation);
+            await ProcessPaymentAsync(command, bankingPaymentId);
+        }
+
+        private async Task<string> SubmitPaymentAsync(PaymentInformation paymentInformation)
+        {
+            try
+            {
+                var paymentId = await _bankingService.MakePayment(paymentInformation);
+                return paymentId;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error processing payment.");
+                throw new BankingException("Error processing payment.", e);
+            }
+        }
+
+        private async Task ProcessPaymentAsync(CreatePayment command, string bankingPaymentId)
+        {
+            try
+            {
+                var payment = new Payment(null,
+                                          bankingPaymentId,
+                                          new CardNumber(command.CardNumber),
+                                          command.ExpityMonth,
+                                          command.ExpiryYear,
+                                          new CVV(command.CVV),
+                                          command.Amount,
+                                          new Currency(command.Currency)
+                );
+
+                await _paymentRepository.CreateAsync(payment);
+                await _paymentRepository.SaveAsync();
+            }
+            catch (Exception e)
+            {
+                _logger.LogCritical(e, "Payment could not be saved after successful payment.", bankingPaymentId);
+                throw e;
+            }
+        }
+    }
+}
